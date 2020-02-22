204f8231f4739072f8d76e69b9d8e7c5feb7a805
Rebecca Pavelka <pavelka6@sbcglobal.net>
2020-02-22 12:22:59 -0600

    Initial commit

---
+++ src/test/java/org/launchcode/InspectorWorkflow/InspectorWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.InspectorWorkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectorWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
