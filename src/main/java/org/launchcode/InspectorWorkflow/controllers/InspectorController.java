c13c5f3dfb19fa94a7abebf7fade6bd92e720ca5
Rebecca Pavelka <pavelka6@sbcglobal.net>
2020-03-14 10:13:03 -0500

    Added landing page and user login template

--- src/main/java/org/launchcode/InspectorWorkflow/controllers/InspectorController.java
+++ src/main/java/org/launchcode/InspectorWorkflow/controllers/InspectorController.java
@@ -1,13 +1,18 @@
 package org.launchcode.InspectorWorkflow.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InspectorController {
 
         @RequestMapping("")
-        public String index() {
+        public String index(Model model) {
+
+            model.addAttribute("title", "Login to access Inspector Database");
+            model.addAttribute("userName");
+            model.addAttribute("password");
 
             return "index";
         }
