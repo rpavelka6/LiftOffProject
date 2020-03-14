9e9e49fdd17f26c8177255fbbf3471917e25d502
Rebecca Pavelka <pavelka6@sbcglobal.net>
2020-03-14 11:30:49 -0500

    Link from landing page to login screen is working

--- src/main/java/org/launchcode/InspectorWorkflow/controllers/InspectorController.java
+++ src/main/java/org/launchcode/InspectorWorkflow/controllers/InspectorController.java
@@ -2,18 +2,20 @@ package org.launchcode.InspectorWorkflow.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("inspector")
 public class InspectorController {
 
-        @RequestMapping("")
+        @GetMapping
         public String index(Model model) {
 
             model.addAttribute("title", "Login to access Inspector Database");
             model.addAttribute("userName");
             model.addAttribute("password");
 
-            return "index";
+            return "inspector/index";
         }
 }
